e0ac89cd316f7df3c33f1b2df349cf3caaead658
Juan C. Montemayor E <juan.montemayor@nytimes.com>
2014-08-31 13:36:22 -0400

    Add extensions to String and Array

--- Mustachio/Mustachio/Extensions.swift
+++ Mustachio/Mustachio/Extensions.swift
@@ -6,4 +6,56 @@
 //  Copyright (c) 2014 jmont. All rights reserved.
 //
 
-import Foundation
+extension String {
+    subscript (r: Range<Int>) -> String {
+        var start = advance(startIndex, r.startIndex)
+        var end = advance(startIndex, r.endIndex)
+        return substringWithRange(Range(start: start, end: end))
+    }
+
+    func length() -> Int {
+        return Array(self).count
+    }
+}
+
+extension Array {
+    func combine(separator: String) -> String{
+        var str : String = ""
+        for (idx, item) in enumerate(self) {
+            str += "\(item)"
+            if idx < self.count-1 {
+                str += separator
+            }
+        }
+        return str
+    }
+
+    func dropWhile(p : T -> Bool) -> [T] {
+        var list = Array<T>()
+        var dropping = true
+        for x in self {
+            if dropping && p(x) {
+                continue
+            } else if dropping {
+                dropping = false
+                list.append(x)
+            } else {
+                list.append(x)
+            }
+        }
+        return list
+    }
+
+    func takeWhile (p : T -> Bool) -> [T] {
+        var list = Array<T>()
+        var taking = true
+        for x in self {
+            if taking && p(x) {
+                list.append(x)
+            } else if taking {
+                break
+            }
+        }
+        return list
+    }
+}
