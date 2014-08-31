28244eb9c1dda26ec25bfa803494b92e2d6f76a7
Juan C. Montemayor E <juan.montemayor@nytimes.com>
2014-08-31 13:36:00 -0400

    Add files to project

---
+++ Mustachio/MustachioTests/ArrayExtensionsTests.swift
@@ -0,0 +1,9 @@
+//
+//  ArrayExtensionsTests.swift
+//  Mustachio
+//
+//  Created by Montemayor Elosua, Juan Carlos on 8/31/14.
+//  Copyright (c) 2014 jmont. All rights reserved.
+//
+
+import Foundation
