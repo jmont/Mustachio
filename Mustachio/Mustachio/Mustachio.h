ac2ba2404958539268da8171669a9ef5cadefc0f
Juan C. Montemayor E <juan.montemayor@nytimes.com>
2014-08-31 10:48:35 -0400

    Initial commit

---
+++ Mustachio/Mustachio/Mustachio.h
@@ -0,0 +1,17 @@
+//
+//  Mustachio.h
+//  Mustachio
+//
+//  Created by Montemayor Elosua, Juan Carlos on 8/31/14.
+//  Copyright (c) 2014 jmont. All rights reserved.
+//
+
+@import UIKit;
+
+//! Project version number for Mustachio.
+FOUNDATION_EXPORT double MustachioVersionNumber;
+
+//! Project version string for Mustachio.
+FOUNDATION_EXPORT const unsigned char MustachioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mustachio/PublicHeader.h>
