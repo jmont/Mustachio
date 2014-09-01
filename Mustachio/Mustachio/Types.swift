9672f22cb2805b4566773cfaf40edffb96c3ef84
Juan C. Montemayor E <juan.montemayor@nytimes.com>
2014-09-01 17:19:18 -0400

    Improve types

--- Mustachio/Mustachio/Types.swift
+++ Mustachio/Mustachio/Types.swift
@@ -6,11 +6,38 @@
 //  Copyright (c) 2014 jmont. All rights reserved.
 //
 
+import Foundation
+
+public enum ContextType {
+    case Str (String)
+    case Dict ([String : Tag])
+    case List ([Tag])
+    case True
+    case False
+}
+
+public func == (lhs: ContextType, rhs: ContextType) -> Bool {
+    switch (lhs, rhs) {
+    case (.Str (let s1), .Str (let s2)):
+        return s1 == s2
+    case (.Dict (let d1), .Dict (let d2)):
+        return d1 == d2
+    case (.List (let l1), .List (let l2)):
+        return l1.count == l2.count && l1 == l2
+    case (.True, .True):
+        return true
+    case (.False, .False):
+        return true
+    default:
+        return false
+    }
+}
+
 public enum Tag : Equatable {
     case Str (String)
     case Variable (String)
     case UnescapedVariable (String)
-    case Section (String, String)
+    case Section (String, ContextType)
     case InvertedSection (String, String)
 }
 
@@ -34,12 +61,4 @@ public func == (lhs: Tag, rhs: Tag) -> Bool {
     default:
         return false
     }
-}
-
-public enum ContextType {
-    case Str (String)
-    case Dict (Dictionary<String, ContextType>)
-    case List (Array<ContextType>)
-    case True
-    case False
 }
